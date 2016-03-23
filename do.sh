6823f8b21ee7a482cc5d024f00baad7e868e7c2f
Patrik Beno <patrikbeno@gmail.com>
2016-03-23 10:30:09 +0000

    do.sh

--- do.sh
+++ do.sh
@@ -14,6 +14,21 @@ publish() {
 	done
 }
 
+run() {
+	local name="$1"; shift;
+	local docker_opts_dflt="-it --rm=true"
+	docker run --name $name ${DOCKER_OPTS:-$docker_opts_dflt} --net=host --cap-add=NET_ADMIN jrevolt/$name "$@"
+}
+
+start() {
+	DOCKER_OPTS="-d" run "$@"
+}
+
+shell() {
+	local name="$1"
+	DOCKER_OPTS="--entrypoint=sh -it --rm=true" run $name -l
+}
+
 
 "$@"
 
